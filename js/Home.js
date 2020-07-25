790a6a21b2fa60cc8b4ff868c4556f5c7fae9722
afzalulabd <afzalul94@gmail.com>
2020-07-26 02:14:38 +0530

    Home Page

--- js/Home.js
+++ js/Home.js
@@ -1,24 +1,23 @@
 var main=document.createElement('div');
-main.classList.add('flex-container');
+main.classList.add('home_container');
 
 document.body.append(main);
 
-var x = document.createElement("IMG");
-x.setAttribute("src", "./img/brain_running.png");
-x.setAttribute("width", "80%");
-x.setAttribute("height", "100%");
-x.setAttribute("alt", "Sequence Finder");
-x.classList.add('gameimage');
-main.appendChild(x);
 
 var game_box=document.createElement('div');
 game_box.classList.add('game_box');
-main.appendChild(game_box);
+document.body.append(game_box);
 
 var gametitle=document.createElement('P');
 gametitle.innerHTML ="SEQUENCE FINDER"
 gametitle.classList.add('gametitle');
 game_box.appendChild(gametitle);
+
+var game_button = document.createElement("BUTTON");
+ var gamenode=document.createTextNode("Let's Play!");
+ game_button.appendChild(gamenode);
+ game_button.classList.add('gamebutton');
+ game_box.appendChild(game_button);
 
 
 
